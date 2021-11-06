4aad57494e6ae2edfbef7dae8cd3f5e6b889afd9
lse1656 <lse1656@naver.com>
2021-11-06 18:55:49 +0900

    🚧

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@ langBtn.addEventListener('click', () => {
   langToggle.classList.toggle('active');
 });
 
+//recom-carousel toggle
+// const recomBtn = document.querySelector('.recom-carousel-control');
+// const recomCarousel = document.querySelector('.recom-carousel');
+
+// recomBtn.addEventListener('click', () => {
+//   recomCarousel.classList.toggle('active');
+// });
+
 // match-service-tab
 const matchItem = document.querySelectorAll('.match-service-tap .menu-item');
 
@@ -19,6 +27,24 @@ for (i = 0; i < matchItem.length; i++) {
   matchItem[i].addEventListener('click', (e) => {
     for (j = 0; j < matchItem.length; j++) {
       matchItem[j].classList.remove('active');
+    }
+    e.target.parentNode.classList.add('active');
+  });
+}
+
+// policy-infor-tab
+const polictItem = document.querySelectorAll('.policy-infor-tab li');
+
+function tabHandler() {
+  for (i = 0; i < polictItem.length; i++) {
+    polictItem[i].classList.add('active');
+  }
+}
+
+for (i = 0; i < polictItem.length; i++) {
+  polictItem[i].addEventListener('click', (e) => {
+    for (j = 0; j < polictItem.length; j++) {
+      polictItem[j].classList.remove('active');
     }
     e.target.parentNode.classList.add('active');
   });
