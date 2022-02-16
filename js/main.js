ff56a87ac901e3b472647b59fb405e78b701d73b
lse1656 <lse1656@naver.com>
2022-02-16 14:44:56 +0900

    📝 크로스 브라우징

--- js/main.js
+++ js/main.js
@@ -2,46 +2,54 @@
 const langBtn = document.querySelector('.lang-btn');
 const langToggle = document.querySelector('.lang-toggle');
 
-langBtn.addEventListener('click', (e) => {
+function langToggleHandler(e) {
   e.preventDefault();
   langToggle.classList.toggle('active');
-});
+}
+
+langBtn.addEventListener('click', langToggleHandler);
 
 //recom-carousel toggle
 const recomControl = document.querySelector('.recom-carousel-control');
 const recomBtn = document.querySelector('.recom-carousel-control .open-btn');
 const recomCont = document.querySelector('.recom-carousel-cont');
 
-recomControl.addEventListener('click', (e) => {
+function recomHandler(e) {
   e.preventDefault();
   recomCont.classList.toggle('active');
   recomBtn.classList.toggle('active');
-});
+}
+
+recomControl.addEventListener('click', recomHandler);
 
 // match-service-tab
 const matchItem = document.querySelectorAll('.match-service-tap .menu-item');
 
-for (i = 0; i < matchItem.length; i++) {
-  matchItem[i].addEventListener('click', (e) => {
-    e.preventDefault();
+function matchServiceHandler(e) {
+  e.preventDefault();
 
-    for (j = 0; j < matchItem.length; j++) {
-      matchItem[j].classList.remove('active');
-    }
-    e.target.parentNode.classList.add('active');
-  });
+  for (j = 0; j < matchItem.length; j++) {
+    matchItem[j].classList.remove('active');
+  }
+  e.target.parentNode.classList.add('active');
+}
+
+for (i = 0; i < matchItem.length; i++) {
+  matchItem[i].addEventListener('click', matchServiceHandler);
 }
 
 // policy-infor-tab
 const policyItem = document.querySelectorAll('.policy-infor-tab li');
 
-for (i = 0; i < policyItem.length; i++) {
-  policyItem[i].addEventListener('click', (e) => {
-    e.preventDefault();
+function policyInforHandler(e) {
+  e.preventDefault();
 
-    for (j = 0; j < policyItem.length; j++) {
-      policyItem[j].classList.remove('active');
-    }
-    e.target.parentNode.classList.add('active');
-  });
+  for (j = 0; j < policyItem.length; j++) {
+    policyItem[j].classList.remove('active');
+  }
+  e.target.parentNode.classList.add('active');
+}
+
+for (i = 0; i < policyItem.length; i++) {
+  policyItem[i].addEventListener('click', policyInforHandler);
 }
